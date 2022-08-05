878b2a85dc669b8c008f969f44c6c0f372a186b2
daniel21connor <jconnorr1@miumg.edu.gt>
2022-08-05 15:22:42 +0200

    se agregaron las bases de la clases y como funcionara el codigo en si

--- src/java/clase/ClienteModel.java
+++ src/java/clase/ClienteModel.java
@@ -1,13 +1,78 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package clase;
 
-/**
- *
- * @author PC GAMING
- */
+
 public class ClienteModel {
     
+  private String nombre;
+  private String apellido;
+  private String codigo;
+  private String numero;
+  private String Correo;
+  private String contraseña;
+private String direccion;
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getCorreo() {
+        return Correo;
+    }
+
+    public void setCorreo(String Correo) {
+        this.Correo = Correo;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+  
+    
+  
+    
+  
+    
+    
+    
+
 }
